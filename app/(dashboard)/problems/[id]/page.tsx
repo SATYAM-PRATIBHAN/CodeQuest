ba72d145c028d28e1c1970e0a1089a1a96b2a423
SATYAM-PRATIBHAN <satyampratibhan@gmail.com>
2025-03-18 22:37:24 +0530

    Updated subission protocol

--- app/(dashboard)/problems/[id]/page.tsx
+++ app/(dashboard)/problems/[id]/page.tsx
@@ -335,8 +335,7 @@ export default function ProblemPage() {
   );
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 min-h-max p-8 bg-gray-950 text-gray-100">
-
+    <div className="flex flex-col md:flex-row min-h-max p-8 bg-gray-950 text-gray-100">
       {/* Left Section: Problem Details */}
       <div className="md:w-1/2 p-8 bg-gradient-to-br from-gray-900 to-gray-950 rounded-2xl shadow-lg border border-gray-700/30">
         <h1 className="text-3xl font-bold text-blue-400 mb-12 tracking-tight">
