84ce75beec5c5ec3d68845ce14aa9d61092bb0a3
SATYAM-PRATIBHAN <satyampratibhan@gmail.com>
2025-03-23 02:23:53 +0530

    added next-pwa

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,12 @@ const nextConfig: NextConfig = {
     env: {
         ADMIN_EMAIL: process.env.ADMIN_EMAIL,
         ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
-      },
+    },
+    pwa: {
+      dest: "public",
+      register: true,
+      skipWaiting: true,
+    },
 };
 
 export default nextConfig;
